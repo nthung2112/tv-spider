fe42c21d81a75f8c26f2dffb2ea347a288ac3654
Hung Nguyen <nthung2112@gmail.com>
2024-05-25 18:55:11 +0700

    Fix thapcamtv name

--- js/thapcamtv.js
+++ js/thapcamtv.js
@@ -1,18 +1,18 @@
-import { _ } from "./lib/cat.js";
-import { showDateText } from "./lib/similarity.js";
+import { _ } from './lib/cat.js';
+import { showDateText } from './lib/similarity.js';
 
-let url = "https://api.thapcam.xyz";
-let siteKey = "";
+let url = 'https://api.thapcam.xyz';
+let siteKey = '';
 let siteType = 0;
 
 const UA =
-  "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1';
 
 async function request(reqUrl, agentSp) {
   let res = await req(reqUrl, {
-    method: "get",
+    method: 'get',
     headers: {
-      "User-Agent": agentSp || UA,
+      'User-Agent': agentSp || UA,
     },
   });
   return res.content;
@@ -25,16 +25,16 @@ async function init(cfg) {
 }
 
 async function home(filter) {
-  const data = JSON.parse(await request(url + "/api/match/featured/mt")).data;
-  const featured = JSON.parse(await request(url + "/api/match/featured")).data;
+  const data = JSON.parse(await request(url + '/api/match/featured/mt')).data;
+  const featured = JSON.parse(await request(url + '/api/match/featured')).data;
   const filterObj = {};
   const groupByStatus = _.groupBy(data, (item) =>
-    featured.some((f) => f.id === item.id) ? "featured" : item.match_status
+    featured.some((f) => f.id === item.id) ? 'featured' : item.match_status
   );
   const mappingType = {
-    featured: "Nổi bật",
-    live: "Đang diễn ra",
-    pending: "Sắp diễn ra",
+    featured: 'Nổi bật',
+    live: 'Đang diễn ra',
+    pending: 'Sắp diễn ra',
   };
   const orders = {
     featured: 0,
@@ -46,11 +46,11 @@ async function home(filter) {
     .map(([typeId, matches]) => {
       const tours = _.groupBy(
         matches.sort((a, b) => b.tournament.priority - a.tournament.priority),
-        "tournament.name"
+        'tournament.name'
       );
       let tag = {
-        key: "tag",
-        name: "Type",
+        key: 'tag',
+        name: 'Type',
         value: Object.entries(tours).map(([tourName]) => {
           return {
             n: tourName,
@@ -60,13 +60,13 @@ async function home(filter) {
       };
 
       if (tag.value.length > 0) {
-        tag["init"] = tag.value[0].v;
+        tag['init'] = tag.value[0].v;
         filterObj[typeId] = [tag];
       }
 
       return {
         type_id: typeId,
-        type_name: mappingType[typeId] || "Không xác định",
+        type_name: mappingType[typeId] || 'Không xác định',
       };
     })
     .sort((a, b) => orders[a.type_id] - orders[b.type_id]);
@@ -78,7 +78,7 @@ async function home(filter) {
 }
 
 async function homeVod() {
-  const matches = JSON.parse(await request(url + "/api/match/featured")).data;
+  const matches = JSON.parse(await request(url + '/api/match/featured')).data;
 
   const list = matches
     .sort((a, b) => b.tournament.priority - a.tournament.priority)
@@ -97,10 +97,10 @@ async function homeVod() {
 
 async function category(tid, pg, filter, extend) {
   let matches = [];
-  if (tid === "featured") {
-    matches = JSON.parse(await request(url + "/api/match/featured")).data;
+  if (tid === 'featured') {
+    matches = JSON.parse(await request(url + '/api/match/featured')).data;
   } else {
-    matches = JSON.parse(await request(url + "/api/match/featured/mt")).data;
+    matches = JSON.parse(await request(url + '/api/match/featured/mt')).data;
 
     if (tid) {
       matches = matches.filter((m) => m.match_status === tid);
@@ -139,15 +139,15 @@ async function detail(id) {
   const vod = {
     vod_id: id,
     vod_pic: meta.tournament.logo,
-    vod_name: `${meta.home.name} ${meta.scores.home} - ${meta.scores.away} ${meta.away.name}`,
-    vod_play_from: !_.isEmpty(data.play_urls) ? "ThapCamTV" : "",
+    vod_name: `${meta.home.name} ${meta.scores.home} - ${meta.scores.away} ${
+      meta.away ? meta.away.name : ''
+    }`,
+    vod_play_from: !_.isEmpty(data.play_urls) ? 'ThapCamTV' : '',
     vod_play_url: !_.isEmpty(data.play_urls)
-      ? data.play_urls.map((item) => `${item.name}$${item.url}`).join("#")
-      : "",
+      ? data.play_urls.map((item) => `${item.name}$${item.url}`).join('#')
+      : '',
     vod_remarks: meta.tournament.name,
-    vod_actor: data.commentators
-      ? data.commentators.map((c) => c.name).join(" vs ")
-      : "",
+    vod_actor: data.commentators ? data.commentators.map((c) => c.name).join(' vs ') : '',
     vod_content: meta.name,
   };
 
@@ -166,9 +166,7 @@ async function play(flag, id, flags) {
 async function search(wd, quick) {
   const data = JSON.parse(await request(url + `/api/match/featured/mt`)).data;
 
-  const matches = data.filter((m) =>
-    m.name.toLowerCase().includes(wd.toLowerCase())
-  );
+  const matches = data.filter((m) => m.name.toLowerCase().includes(wd.toLowerCase()));
 
   let list = _.map(matches, (item) => {
     return {
