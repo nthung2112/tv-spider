198cc09bedf97ce8759344c97c42c9081f57834e
Hung Nguyen <nthung2112@gmail.com>
2025-01-12 14:20:39 +0700

    Add spider interface

---
+++ src/spider.ts
@@ -0,0 +1,32 @@
+interface Context {
+  skey: string;
+  stype: number;
+  ext?: string;
+}
+
+export interface Spider {
+  init(cfg: Context): Promise<void>;
+
+  home(filter: boolean): Promise<Stringified<HomeData>>;
+
+  homeVod(): Promise<Stringified<VodData>>;
+
+  category(
+    tid: string,
+    page: string,
+    filter: boolean,
+    extend: Record<string, string>
+  ): Promise<Stringified<CategoryData>>;
+
+  detail(id: string): Promise<Stringified<VodData>>;
+
+  play(flag: string, id: string, vipFlags: string[]): Promise<Stringified<PlayData>>;
+
+  search(key: string, quick?: boolean, page?: string): Promise<Stringified<CategoryData>>;
+
+  live(url: string): Promise<string>;
+
+  action(action: string): Promise<string>;
+
+  destroy(): void;
+}
