f457d447fa81e7025d0eb25bc590ac3ca1e2a16b
Hung Nguyen <nthung2112@gmail.com>
2026-02-08 13:48:21 +0700

    Test new bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
   clean: true,
   dts: false,
-  entry: ['./src/sites/*.ts'],
-  format: ['esm'],
+  entry: ["./src/sites/*.ts"],
+  format: ["esm"],
   sourcemap: false,
-  minify: true,
-  target: 'es2020',
+  minify: false,
+  target: "es2020",
   splitting: false,
-  outDir: 'dist',
-  platform: 'browser',
+  outDir: "dist",
+  platform: "browser",
+  external: ["cheerio"],
 });
